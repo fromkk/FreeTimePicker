730b616c53f1f0c002e9d507dc5a06e0d95525cc
Kazuya Ueoka <info@fromkk.info>
2020-02-06 15:25:51 +0900

    Initial Commit

---
+++ TimePicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimePicker
+//
+//  Created by Kazuya Ueoka on 2020/02/06.
+//  Copyright © 2020 fromKK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
