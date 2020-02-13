e066684bd9548f823f14e1d7be64250b5e62bf79
Kazuya Ueoka <info@fromkk.info>
2020-02-13 17:12:14 +0900

    add toSearchDateType

--- TimePicker/Views/DateTypeView/SearchDateType+Intents.swift
+++ TimePicker/Views/DateTypeView/SearchDateType+Intents.swift
@@ -27,3 +27,24 @@ extension SearchDateType {
         }
     }
 }
+
+extension DateType {
+    func toSearchDateType() -> SearchDateType? {
+        switch self {
+        case .today:
+            return .today
+        case .tomorrow:
+            return .tomorrow
+        case .thisWeek:
+            return .thisWeek
+        case .nextWeek:
+            return .nextWeek
+        case .thisMonth:
+            return .thisMonth
+        case .nextMonth:
+            return .nextMonth
+        case .unknown:
+            return nil
+        }
+    }
+}
