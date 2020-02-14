4006762ec050c0e108c2ed9e4769978d0c0ac7aa
Kazuya Ueoka <info@fromkk.info>
2020-02-15 08:55:16 +0900

    add contains test

--- CoreTests/EventDateCalculatorTests.swift
+++ CoreTests/EventDateCalculatorTests.swift
@@ -6,11 +6,39 @@
 //  Copyright © 2020 fromKK. All rights reserved.
 //
 
-@testable import FreeTimePicker
+@testable import Core
 import XCTest
 
 final class EventDateCalculatorTests: XCTestCase {
-    func testIsContains() {}
+    let calculator = EventDateCalculator()
+    
+    func testIsContains() {
+        XCTContext.runActivity(named: "isContains entity and entity") { (_) in
+            let baseEntity = EventEntity(title: "hoge", startDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 10, minute: 30), endDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 19, minute: 30), isAllDay: false)
+            let distEntity = EventEntity(title: "hoge", startDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 11, minute: 30), endDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 18, minute: 30), isAllDay: false)
+            XCTAssertTrue(calculator.isContains(source: baseEntity, dist: distEntity))
+        }
+        
+        XCTContext.runActivity(named: "!isContains entity and entity") { (_) in
+            let baseEntity = EventEntity(title: "hoge", startDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 11, minute: 30), endDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 18, minute: 30), isAllDay: false)
+            let distEntity = EventEntity(title: "hoge", startDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 10, minute: 30), endDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 19, minute: 30), isAllDay: false)
+            XCTAssertFalse(calculator.isContains(source: baseEntity, dist: distEntity))
+        }
+        
+        XCTContext.runActivity(named: "isContains entity and date") { (_) in
+            let baseEntity = EventEntity(title: "hoge", startDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 10, minute: 30), endDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 19, minute: 30), isAllDay: false)
+            let date = DateCreator.create(year: 2020, month: 2, day: 20, hour: 12, minute: 0)
+            XCTAssertTrue(calculator.isContains(source: baseEntity, at: date))
+        }
+        
+        XCTContext.runActivity(named: "!isContains entity and date") { (_) in
+            let baseEntity = EventEntity(title: "hoge", startDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 10, minute: 30), endDate: DateCreator.create(year: 2020, month: 2, day: 20, hour: 19, minute: 30), isAllDay: false)
+            let date1 = DateCreator.create(year: 2020, month: 2, day: 20, hour: 10, minute: 29)
+            let date2 = DateCreator.create(year: 2020, month: 2, day: 20, hour: 19, minute: 31)
+            XCTAssertFalse(calculator.isContains(source: baseEntity, at: date1))
+            XCTAssertFalse(calculator.isContains(source: baseEntity, at: date2))
+        }
+    }
     func testIsIntersects() {}
     func testTimeInterval() {}
     func testStartTimeInterval() {}
--- CoreTests/FreeTimeConverterTests.swift
+++ CoreTests/FreeTimeConverterTests.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 fromKK. All rights reserved.
 //
 
-@testable import FreeTimePicker
+@testable import Core
 import XCTest
 
 final class FreeTimeConverterTests: XCTestCase {
