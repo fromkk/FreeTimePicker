cc65f8f20f863aec8986c4f303c48f3cce09f926
Kazuya Ueoka <info@fromkk.info>
2020-02-15 21:34:32 +0900

    fixed update_builds

--- scripts/update_builds.sh
+++ scripts/update_builds.sh
@@ -5,9 +5,6 @@ BUILD_NUMBER=$(git log --oneline | wc -l | tr -cd '0123456789')
 
 for ((i = 0; i < ${#INFO_PLISTS[@]}; i++)); do
 	plist=${INFO_PLISTS[$i]}
-	if [ -f /usr/local/bin/SpellChecker ]; then
-		/usr/libexec/PlistBuddy $plist -c "set CFBundleVersion $BUILD_NUMBER"
-	fi
 	/usr/libexec/PlistBuddy $plist -c "set CFBundleVersion $BUILD_NUMBER"
 done
 
