fa5a1bf9dc9ac456a1e36cb9ba82882e794387e6
Vova X <vovax87@gmail.com>
2021-01-21 11:18:06 +0200

    testcicd

---
+++ TesyCiCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesyCiCD
+//
+//  Created by Vova X on 21/01/2021.
+//  Copyright © 2021 Vova X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
